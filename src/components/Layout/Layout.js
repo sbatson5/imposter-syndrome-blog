da2cd13bc8e2c3421ee18b520b171b8159e0edad
Scott Batson <sjbats@email.com>
2019-06-07 11:34:09 -0400

    add g-suite tag

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,7 +13,8 @@ const Layout = ({
       <html lang="en" />
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta property="og:image" content={image} />
+      <meta name="google-site-verification" content="KcmxLbVeRmb_f539GPdSV2ZHVl9eqV9qEXclRMLC1MA" />
+      {image ? <meta property="og:image" content={image} /> : ''}
       <meta property="og:type" content="blog" />
     </Helmet>
     {children}
