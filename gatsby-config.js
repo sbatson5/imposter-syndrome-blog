069ac331b72c0e2f144516a0d53a3b486d9729f5
Scott Batson <sbatson5@gmail.com>
2021-11-28 12:38:08 -0500

    update google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,20 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "UA-141208714-1",
-        head: true
+        trackingIds: ["UA-141208714-1"],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
       },
     },
     `gatsby-plugin-react-helmet`,
